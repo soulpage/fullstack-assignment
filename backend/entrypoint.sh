565e5e99451437a13967f1a77fb4793ae9c5f4bb
saikrishna007 <149296081+saikrishna0126@users.noreply.github.com>
2024-05-26 17:11:59 +0530

    Create entrypoint.sh

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Main entry point script for the Docker container
+
+# Wait for SQLite to be ready
+/wait-for-sqlite.sh python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Django application with Gunicorn
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
