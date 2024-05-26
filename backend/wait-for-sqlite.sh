38a4636f2873bc4c5e499039713e1b2901c384ec
saikrishna007 <149296081+saikrishna0126@users.noreply.github.com>
2024-05-26 17:13:19 +0530

    Create wait-for-sqlite.sh

---
+++ backend/wait-for-sqlite.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Script to wait for SQLite to be ready
+
+set -e
+
+until [ -e /app/backend/db.sqlite3 ]; do
+    >&2 echo "SQLite is not ready - sleeping"
+    sleep 1
+done
+
+>&2 echo "SQLite is ready - executing command"
+exec "$@"
