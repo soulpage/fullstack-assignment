fdabaf95b93cc82bc753dce781e880315dd9bf7d
Dhana180 <120277474+Dhana180@users.noreply.github.com>
2024-06-18 16:17:52 +0000

    task1 created --

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -13,6 +13,7 @@ class Role(models.Model):
 
 
 class Conversation(models.Model):
+    summary=''
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100, blank=False, null=False, default="Mock title")
     created_at = models.DateTimeField(auto_now_add=True)
