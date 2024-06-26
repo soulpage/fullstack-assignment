73b67fb2900fd4b9d88270606e79be5ff8f732a0
Abhayaj247 <abhayjoshi2830@gmail.com>
2024-06-26 15:26:20 +0530

    Task 2: 1)Migrated the database from SQLite to PostgreSQL.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#Migrated the database from SQLite to PostgreSQL.
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
