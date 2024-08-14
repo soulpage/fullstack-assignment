4f3be058d8999463ed127f9c6174fe7f21d98685
Nikhil Purohit <npurohit00@gmail..com>
2024-08-14 17:39:12 +0530

    added all hosts in setings.py file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ FRONTEND_URL = os.environ["FRONTEND_URL"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
