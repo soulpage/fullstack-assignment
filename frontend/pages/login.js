52b34bfa8bd7d41059d4a0df9cf3fe1146766cb1
npurohit00 <npurohit00@gmail.com>
2024-08-15 12:54:02 +0530

    login name changed

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -14,7 +14,7 @@ function LoginPage() {
                 <div className={styles.formContainer}>
                     <div className={styles.formHeader}>
                         <LockKeyIcon/>
-                        <h1>Login Nikhil purohit
+                        <h1>Login Nikhil purohit roman
                         </h1>
                     </div>
                     <LoginForm isSubmitting={isSubmitting} setIsSubmitting={setIsSubmitting}/>
