2c8f0dbc60248b3a1169afdb304192d48d9977d9
Kandukuri saikrishna <50510272+KANDUKURIsaikrishna@users.noreply.github.com>
2024-08-29 19:32:31 +0200

    db_change_3

--- db/init_design.sql
+++ db/init_design.sql
@@ -1,28 +1,33 @@
-CREATE TABLE Role (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL
-);
 
-CREATE TABLE Conversation (
-    id UUID PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    created_at TIMESTAMPTZ,
-    modified_at TIMESTAMPTZ,
-    active_version_id UUID REFERENCES Version(id),
-    active BOOLEAN DEFAULT FALSE
-);
+Table Role
+{
+  id integer [primary key]
+  name varchar(20) [not null]
+}
 
-CREATE TABLE Version (
-    id UUID PRIMARY KEY,
-    root_message_id UUID REFERENCES Message(id),
-    conversation_id UUID NOT NULL REFERENCES Conversation(id),
-    parent_version_id UUID REFERENCES Version(id)
-);
+Table Version
+{
+  root_message_id UUID [null, ref: > Message.id]
+  conversation_id UUID [not null, ref: > Conversation.id]
+  parent_version_id UUID [null, ref: > Version.id]
+  id UUID [primary key]
+}
 
-CREATE TABLE Message (
-    id UUID PRIMARY KEY,
-    content TEXT NOT NULL,
-    role_id INTEGER NOT NULL REFERENCES Role(id),
-    created_at TIMESTAMPTZ,
-    version_id UUID NOT NULL REFERENCES Version(id)
-);
+Table Conversation
+{
+  id UUID [primary key]
+  title varchar(100) [not null]
+  created_at datetime
+  modified_at datetime
+  active_version_id UUID [null, ref: > Version.id]
+  active boolean [default: false]
+}
+
+Table Message
+{
+  id UUID [primary key]
+  content text [not null]
+  role_id UUID [not null, ref: > Role.id]
+  created_at datetime
+  version_id UUID [not null, ref: > Version.id]
+}
