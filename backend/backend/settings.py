bd7a4ee8a5e36fc24dc4b043d80d6ee33cf2c277
Kandukuri saikrishna <50510272+KANDUKURIsaikrishna@users.noreply.github.com>
2024-08-29 15:55:55 +0200

    db_change_2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
