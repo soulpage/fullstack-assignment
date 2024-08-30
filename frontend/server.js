e14af3d5513762503b71bd3e0d3a1f9b655af136
Kandukuri saikrishna <50510272+KANDUKURIsaikrishna@users.noreply.github.com>
2024-08-30 03:06:10 +0200

    server

--- frontend/server.js
+++ frontend/server.js
@@ -10,6 +10,7 @@ app.get('/metrics', async (req, res) => {
   res.end(await client.register.metrics());
 });
 
-app.listen(3000, () => {
-  console.log('Next.js app running on port 3000');
+const PORT =process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}`');
 });
